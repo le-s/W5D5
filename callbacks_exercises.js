f02e79bcbdd2983b448fd24db599a784fcc8608d
le-s <steven.an.le@gmail.com>
2018-11-23 14:12:08 -0800

    Complete addNumbers

--- callbacks_exercises.js
+++ callbacks_exercises.js
@@ -27,5 +27,30 @@ class Clock {
 
   }
 }
+//
+// const clock = new Clock();
 
-const clock = new Clock();
+
+// addNumbers
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft === 0) {
+    return completionCallback(sum);
+  }
+
+  rl.question("Please input a number: ", function (answer) {
+    let num = parseInt(answer);
+    sum += num;
+    console.log(sum);
+    addNumbers(sum, numsLeft - 1, completionCallback);
+  });
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
